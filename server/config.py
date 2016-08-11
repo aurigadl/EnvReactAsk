aef867d4b55d1e15d583e1ad0730de6429bf8579
aurigadl <aurigadl@gmail.com>
2016-08-11 10:12:45 -0500

    Delete server from repo with more front-end

--- server/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DEBUG = True
-RBAC_USE_WHITE = True
-TOKEN_SECRET = os.environ.get('SECRET_KEY') or os.urandom(24)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-SQLALCHEMY_ECHO = False
