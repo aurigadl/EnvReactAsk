56fcc3d13d920db3b5241411ba01e8f868b7e4b4
aurigadl <aurigadl@gmail.com>
2016-09-21 21:34:49 -0500

    revoke --no-assume-unchanged  for config.py

--- server/config.py
+++ server/config.py
@@ -1,7 +1,18 @@
 import os
 
-DEBUG = True
-RBAC_USE_WHITE = True
-TOKEN_SECRET = os.environ.get('SECRET_KEY') or os.urandom(24)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-SQLALCHEMY_ECHO = False
+class config(object):
+    DEBUG = False
+    RBAC_USE_WHITE = True
+    TOKEN_SECRET = os.environ.get('SECRET_KEY') or os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_ECHO = False
+    SECRET_KEY = str(os.urandom(24)) + 'SRT#$ASDF_;:' + str(os.urandom(24))
+
+class devConfig1(config):
+    DEBUG = True
+    SQLALCHEMY_ECHO = False
+    SECRET_KEY = str(os.urandom(4)) + 'SRT#$ASDF_;:' + str(os.urandom(4))
+
+class devConfig2(config):
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
